1d028c40be8b7cd7e0e8b6f1eed832fba9e64eb0
Mario <mario.prskalo5@gmail.com>
2024-11-11 22:09:05 +0100

    half of fundamentals part 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,140 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Mario";
+let age = "23";
+
+console.log(`My name is ${firstName} and I am ${age} years old`);
+let myFirstJob = "Web Developer";
+
+console.log(myFirstJob);
+
+
+); */
+
+/* let javascriptIsFun = true;
+
+console.log(javascriptIsFun);
+
+console.log(typeof 23);
+
+console.log(typeof javascriptIsFun);
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2001;
+console.log(typeof year);
+
+console.log(typeof null);
+
+let country = "Croatia";
+let continent = "Europe";
+let population = 3200000;
+let isIsland = false;
+let language;
+
+console.log(
+  typeof isIsland,
+  typeof population,
+  typeof country,
+  typeof language
+);*/
+
+/* let age = 30;
+age = 31;
+
+// const birthYear = 2001;
+
+let lastName = "Prskalo";
+
+console.log(lastName);
+
+let country = "Croatia";
+const continent = "Europe";
+let population = 3200000;
+const isIsland = false;
+const language = "Croatian"; */
+
+/* //Math Operators
+let currentYear = 2077;
+
+let ageMario = currentYear - 2001;
+let ageSara = currentYear - 2028;
+console.log(ageMario, ageSara);
+
+console.log(ageMario * 2, ageMario / 10, 2 ** 3);
+// 2**3 means 2 to the power of 3 which is = 2*2*2
+
+const firstName = "Mario";
+const lastName = "Prskalo";
+
+console.log(firstName + " " + lastName);
+
+// Comparison operators
+console.log(ageMario < ageSara); // >, <, >=, <=
+
+console.log(ageSara >= 18);
+
+const isFullAge = ageSara >= 18;
+console.log(currentYear - 2001 > currentYear - 2028);
+
+// Assigment operators
+let x = 10 + 5; //15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x); 
+
+*/
+/*
+let currentYear = 2077;
+
+let ageMario = currentYear - 2001;
+let ageSara = currentYear - 2028;
+
+console.log(ageMario > ageSara);
+console.log(currentYear - 2001 > currentYear - 2028);
+
+const averageAge = (ageMario + ageSara) / 2;
+console.log(ageMario, ageSara, averageAge);
+
+// Assignment
+let country = "Croatia";
+const continent = "Europe";
+let population = 3200000;
+const isIsland = false;
+const language = "Croatian";
+
+let halfPopulation = population / 2;
+console.log((population += 1));
+let finlandPopulation = 6000000;
+let isGreaterPopulation = population > finlandPopulation;
+let averagePopulation = 33000000;
+let isLessThanAveragePopulation = population < averagePopulation;
+
+const description = `${country} is in ${continent}, and its ${population} people speak ${language}`;
+console.log(description); 
+*/
+
+/* 
+// Challenge (Maybe change the let's to const's)
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.88;
+
+const BMIMark = massMark / (heightMark * heightMark)
+
+const BMIJohn = massJohn / (heightJohn * heightJohn)
+
+let markHigherBMI = BMIMark > BMIJohn 
+
+console.log(BMIMark, BMIJohn, markHigherBMI)
+*/
