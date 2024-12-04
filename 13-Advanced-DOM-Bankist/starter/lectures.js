1d972d90dc2a470c7983f9869caf57aeb6b6acb6
Mario <mario.prskalo5@gmail.com>
2024-12-05 00:23:34 +0100

    implemented smooth scrolling

--- 13-Advanced-DOM-Bankist/starter/lectures.js
+++ 13-Advanced-DOM-Bankist/starter/lectures.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // !Selecting, Creating and Deleting Elements
-
+/*
 // console.log(document.documentElement); // html element
 // console.log(document.head); // head element
 // console.log(document.body); // body element
@@ -24,7 +24,7 @@ const allButtons = document.getElementsByTagName('button'); // this is an HTMLCo
 const message = document.createElement('div');
 message.classList.add('cookie-message');
 // message.textContent = `We use cookies for improved functionality and analytics`;
-message.innerHTML = `We use cookies for improved functionality and analytics <button class ="btn btn--close--cookie">Got it!</button>`;
+message.innerHTML = `We use cookies for improved functionality and analytics <button class ="btn btn--close-cookie">Got it!</button>`;
 
 // header.prepend(message);
 header.append(message);
@@ -36,12 +36,13 @@ header.append(message);
 // !Delete elements
 
 document
-  .querySelector('.btn--close--cookie')
+  .querySelector('.btn--close-cookie')
   .addEventListener('click', () => message.remove()); // old way: message.parentElement.removeChild(message)
-
+*/
+/*
 //! Styles, Attributes and Classes
 message.style.backgroundColor = '#37383d';
-message.style.width = '120%';
+// message.style.width = '120%';
 console.log(message.style.height); // logs nothing because style is set and hidden inside of css
 console.log(message.style.backgroundColor); // logs the color because we set it using javascript
 
@@ -50,4 +51,38 @@ console.log(getComputedStyle(message).color); //
 console.log(getComputedStyle(message).height); //
 
 message.style.height =
-  Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
+  Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+
+// changing document (root) css variables
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes - eg. src, href, alt
+const logo = document.querySelector('.nav__logo');
+// logo.alt = 'Beautiful minimalist logo';
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+// non standard
+// logo.setAttribute('company', 'Bankist');
+// console.log(logo.getAttribute('company'));
+// console.log(logo.company);
+
+// FOR THE RELATIVE SRC FILE PATH
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href); // http://127.0.0.1:8080/#
+console.log(link.getAttribute('href')); // #
+*/
+
+// !Data attributes
+// console.log(logo.dataset.versionNumber);
+
+// Classes
+// logo.classList.add();
+// logo.classList.remove();
+// logo.classList.toggle();
+// logo.classList.contains();
+
+// Dont use logo.className = 'mario' // overwrites all of classes
--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,37 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Smooth scrolling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', e => {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  // console.log(
+  //   `Current scroll (X/Y): ${window.pageXOffset}/${window.pageYOffset}` // deprecated
+  // );
+  console.log(`Current scroll (X/Y): ${window.scrollX}/${window.scrollY}`);
+
+  // scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.scrollX,
+  //   s1coords.top + window.scrollY
+  // );
+  // old school way
+  // window.scrollTo({
+  //   left: s1coords.left + window.scrollX,
+  //   top: s1coords.top + window.scrollY,
+  //   behavior: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// console.log(
+//   `height/width viewport: ${document.documentElement.clientHeight}/${document.documentElement.clientWidth}`
+// );
