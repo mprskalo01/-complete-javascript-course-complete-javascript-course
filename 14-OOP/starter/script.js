fc6a6fff08281b757308acd803887f58da8a23e0
Mario <mario.prskalo5@gmail.com>
2024-12-09 23:04:00 +0100

    OOP theory + plus javascript basics

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Constructor Functions and the new Operator
+
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this - Never create a method using a constructor function
+  // this.calcAge = function () {
+  //   return 2024 - this.birthYear;
+  // };
+};
+
+const mario = new Person('Mario', 2001);
+console.log(mario);
+
+// 1. New {} - empty object is created
+// 2. function is called, this = {} - this is set to the new empty object
+// 3. {} object is linked to prototype
+// 4. function automatically returns the object {...}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(mario instanceof Person); // true
