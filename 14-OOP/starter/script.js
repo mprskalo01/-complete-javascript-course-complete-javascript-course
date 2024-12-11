75de804fdd11a229ed84cde69a4045282a1023bc
Mario <mario.prskalo5@gmail.com>
2024-12-11 17:04:19 +0100

    setters and getters, static methods and object.create + a challenge

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -84,7 +84,7 @@ console.dir(x => x + 1);
 3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
 4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
 
-DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 1: 'FORD' going at 120 km/h
 DATA CAR 2: 'Mercedes' going at 95 km/h
 
 GOOD LUCK 😀
@@ -103,11 +103,11 @@ Car.prototype.brake = function () {
   console.log(`${this.make} is going at ${this.speed} km/h`);
 };
 
-const bmw = new Car('BMW', 120);
+const ford = new Car('FORD', 120);
 const mercedes = new Car('Mercedes', 95);
 
-bmw.accelerate();
-bmw.brake();
+ford.accelerate();
+ford.brake();
 
 mercedes.accelerate();
 mercedes.brake();
@@ -119,9 +119,10 @@ mercedes.brake();
 // const PersonCl = class{}
 
 // class declaration
+/*
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
   // everything outside of constructor will be added to .prototype property
@@ -132,9 +133,24 @@ class PersonCl {
   greet() {
     console.log(`Hey ${this.firstName}`);
   }
+  // GETTER
+  get age() {
+    return 2024 - this.birthYear;
+  }
+
+  // SETTER FOR DATA VALIDATION - setting a property that already exists with the _ and getter
+  set fullName(name) {
+    console.log(name);
+
+    if (name.includes(' ')) this._fullName = name; // convection for underscore
+    else alert(`${name} is not a full name`);
+  }
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
+const jessica = new PersonCl('Jessica Davis', 1996);
 console.log(jessica);
 
 jessica.calcAge();
@@ -148,3 +164,176 @@ jessica.greet();
 // 1. Classes are NOT hoisted -- cannot use them before they are declared
 // 2. Classes are first-class citizens -- we can pass them into functions and return them from functions
 // 3. Classes are executed in strict mode -- body of class is always executed in the strict mode
+
+const walter = new PersonCl('Walter White', 1965);
+
+// Setters and Getters
+
+const account = {
+  owner: 'Mario',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest); // dont need to call the getter
+
+// setting the setter
+account.latest = 50;
+console.log(account.movements);
+console.log(jessica.age); // 28
+*/
+
+// Static methods
+// example of static methods Array.from(), Number.parseFloat() - they are static because they only work on the constructors
+// that means that .from is available on Array constructor, but not an example of array like [1, 4, 5].from()
+/*
+// Constructor function
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this - Never create a method using a constructor function
+  // this.calcAge = function () {
+  //   return 2024 - this.birthYear;
+  // };
+};
+
+// This is a static method for a constructor 'class', the static method is not inherited
+Person.hey = function () {
+  console.log(`Hey there 🙋‍♂️`);
+  // console.log(this);
+};
+
+Person.hey();
+
+// Class
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  // everything outside of constructor will be added to .prototype property
+  calcAge() {
+    console.log(2024 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+  // GETTER
+  get age() {
+    return 2024 - this.birthYear;
+  }
+
+  // SETTER FOR DATA VALIDATION - setting a property that already exists with the _ and getter
+  set fullName(name) {
+    console.log(name);
+
+    if (name.includes(' ')) this._fullName = name; // convection for underscore
+    else alert(`${name} is not a full name`);
+  }
+  get fullName() {
+    return this._fullName;
+  }
+  // the static method is classes is added using the static keyword
+  static hey() {
+    console.log(`Hey there 🙋‍♂️`);
+    // console.log(this);
+  }
+}
+
+PersonCl.hey();
+*/
+// Object.create method --
+/*
+const PersonProto = {
+  calcAge() {
+    console.log(2024 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto); // object steven prototype is equal to the PersonProto
+
+console.log(steven);
+
+steven.name = 'Steven';
+steven.birthYear = '2002';
+steven.calcAge(); // works
+console.log(steven);
+
+console.log(steven.__proto__ === PersonProto); // true
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 2000);
+console.log(sarah);
+*/
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+/*
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+  set speedUS(mph) {
+    this.speed = mph * 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 110);
+
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.brake();
+console.log(ford.speedUS);
+ford.speedUS = 100;
+console.log(ford);
+*/
