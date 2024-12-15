6317a023ce68276b63f49632a87677d7b98e247b
Mario <mario.prskalo5@gmail.com>
2024-12-15 23:39:50 +0100

    Callback hell and its solution: Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -8,7 +8,7 @@ const countriesContainer = document.querySelector('.countries');
 // Asynchronous JavaScript, AJAX and APIs
 
 // Our first AJAX Call : XMLHttpRequest
-
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
@@ -39,8 +39,91 @@ const getCountryData = function (country) {
     countriesContainer.style.opacity = 1;
   });
 };
+*/
 
-getCountryData('portugal')
-getCountryData('usa')
-getCountryData('germany')
+// Welcome to Callback Hell
+/*
+const renderCountry = function (data, className = '') {
+  const html = `
+        <article class="country ${className}">
+          <img class="country__img" src="${data.flags.svg}"/>
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} (mil) population</p>
+            <p class="country__row"><span>🗣️</span>${
+              Object.values(data.languages)[0]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              Object.values(data.currencies)[0].name
+            }</p>
+          </div>
+        </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
+const getCountryAndNeighbor = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const neighbour = data.borders?.[0];
+    if (!neighbour) return;
+
+    // AJAX call neighbour country (2)
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // Render neighbour country (2)
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbor('portugal');
+getCountryAndNeighbor('usa');
+
+// this is nested callback hell - triangle shape
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      setTimeout(() => {
+        console.log('4 seconds passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+*/
+
+// Promises and the Fetch API
+
+const country = 'croatia';
+
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+// request.send();
+
+// fetch API which returns a promise 
+const request = fetch(`https://restcountries.com/v3.1/name/${country}`); // fetch returns a promise
+console.log(request);
