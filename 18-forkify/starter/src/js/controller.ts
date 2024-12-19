a569679d461bd9a1c03d05f38d115a0ca2f0e723
Mario <mario.prskalo5@gmail.com>
2024-12-19 18:48:04 +0100

    Will pivot to learning and mastering TypeScript, and then do the final project in typescript

--- 18-forkify/starter/src/js/controller.ts
+++ 18-forkify/starter/src/js/controller.ts
@@ -1,6 +1,6 @@
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
+const timeout = function (s: number) {
   return new Promise(function (_, reject) {
     setTimeout(function () {
       reject(new Error(`Request took too long! Timeout after ${s} second`));
